56c8a30f5de69dbf76915a0c7d4ffce5ffe67c7d
Christopher Holt <holt.christopher1@gmail.com>
2022-08-13 10:21:02 +1000

    fixed some errors on my end

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,17 @@
-<<<<<<< HEAD
 import Streetview from "./Streetview";
 import Map from "./Map";
-=======
 import { useState, useEffect } from 'react'
 import { supabase } from './client'
->>>>>>> fe61ac65d3466564c133d90fa328ffe08101a4a6
 
 function App() {
   const [scores, setScores] = useState([])
   
   
   useEffect(() => {
-    fetchScors()
+    fetchScore()
   }, []);
 
-  async function fetchScors() {
+  async function fetchScore() {
     const { data } = await supabase
     .from('scores')
     .select()
@@ -24,13 +21,8 @@ function App() {
  
   return (
     <div className="App">
-<<<<<<< HEAD
       <Streetview />
       <Map />
-=======
-      <h1>GeoWhere coming soon</h1>
-      
->>>>>>> fe61ac65d3466564c133d90fa328ffe08101a4a6
     </div>
   );
 }
