153ccb1d25145a430ca32bd9ec8c827eede15355
David Dam <dkdam1302@gmail.com>
2022-08-17 11:30:21 +1000

    impoved the guessmap hover

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -33,7 +33,7 @@ function Home() {
     return (
 
       <div className="login">
-        <h1>Welcome to GoogleWhere?</h1>
+        <h1>Welcome to GeoWhere?</h1>
         <div className="w-full max-w-xs">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
