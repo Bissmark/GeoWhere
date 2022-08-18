549e12e0d0846f8cd50d3cbd7f70fc3cb9cced35
David Dam <dkdam1302@gmail.com>
2022-08-18 23:54:26 +1000

    modified images and finishing touches

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import HomeBackground from './../HomeBackground.jpeg';
+import HomeBackground from './../taclan-world.gif';
 //import logo from './logo.jpeg';
 
 function Home() {
@@ -8,14 +8,19 @@ function Home() {
 
     return (
       <div className="flex justify-center flex-col my-10 text-center">
-        <h1 className='text-yellow-500 text-2xl'>Welcome to GeoWhere?</h1>
+        <div className="mb-7">
+        <h1 className='title text-yellow-500'>Welcome to GeoWhere?</h1>
         <p className='text-yellow-500'>Click below to start playing</p>
+        </div>
+        
         <img src={HomeBackground} alt="Picture of Globe"></img>
-        <h1 className='text-yellow-500'>How to play</h1>
-        <p className='text-yellow-500'>The game mode consists of five rounds, each showing a different street view location.</p>
+      <div className="tutorial">
+        <h1 className='text-yellow-500 text-3xl'>How to play</h1>
+        <p className='text-yellow-500'>The game mode consists of five rounds, each showing a different street view location.
+        </p>
         <br />
         <p className="text-yellow-500">The player scores more points depending on how close to the actual photo location they guess.</p>
-        <br />
+      </div>
         <Link className="mt-5 bg-yellow-400 hover:bg-orange-700 active:animate-ping text-slate font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" to="/PlayTrip">Play</Link>
       </div>
     );
--- src/Components/Quizform.js
+++ src/Components/Quizform.js
@@ -5,29 +5,31 @@ import { Link } from "react-router-dom";
 const QuizForm = () => {
     return (
     <><>
-        <div className="text-white">
-            <h1>Host a quiz</h1>
-            <h4>Take on the role as host, and create an event with both locations and quizzes. Who will come out on top?</h4>
-
-
+        <div className="text-yellow-500 text-center my-10">
+            <h1 className="quizHeading">Host a quiz</h1>
+            <h4>Take on the role as host, and create an event with both locations and quizzes. Who will come out on top?
+            </h4>
+        </div>
+        <div className="quiz text-yellow-500 text-center my-10">
             <img src="https://www.geoguessr.com/_next/static/images/onboarding-1-2x-58eaef4035cf1e4155cdbd187a530895.png" alt="globe picture" />
-            <h2>Host your own game show</h2>
+            <h2 className="quizHeading2">Host your own game show</h2>
             <h4>Create custom quiz games and invite up to 1000 people</h4>
         </div>
     </><>
-        <div className="text-white">
+        <div className="quiz text-yellow-500 text-center my-10">
             <img src="https://www.geoguessr.com/_next/static/images/onboarding-2-2x-45f377de5546d7a29c49c5c5202a0cf2.png" alt="thumbs up" />
-            <h2>No account needed</h2>
+            <h2 className="quizHeading2">No account needed</h2>
             <h4>Anyone can join with any device in a matter of seconds.</h4>
         </div>
     </><>
-        <div className="text-white">
+        <div className="quiz text-yellow-500 text-center my-10">
             <img src="https://www.geoguessr.com/_next/static/images/onboarding-3-2x-95a89d5b0cb37f58ee54bf5a165986bd.png" alt="messege" />
-            <h2>Customize the quiz</h2>
+            <h2 className="quizHeading2">Customize the quiz</h2>
             <h4>Add fun questions or pick particular locations around the world.</h4>
         </div>
     </>
-    <Link className="mt-5 bg-yellow-400 hover:bg-orange-700 active:animate-ping text-slate font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" to= "/QuizParent">Create Quiz</Link>
+    <></>
+    <Link className="quizButton bg-yellow-400 hover:bg-orange-700 active:animate-ping text-slate font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" to= "/QuizParent">Create Quiz</Link>
     </>
     
     );
